bc0563e36ca9aba6712fbb9650e72894153d7926
dss16694 <dss16694@163.com>
2017-10-09 15:19:17 +0800

    Update for wechat 6.5.16

--- app/src/main/java/com/yyxx/wechatfp/ObfuscationHelper.java
+++ app/src/main/java/com/yyxx/wechatfp/ObfuscationHelper.java
@@ -11,10 +11,13 @@ public class ObfuscationHelper {
         public static Class<?> PayUI,FetchUI,Payview,WalletBaseUI;
 
         private static void init(int idx, LoadPackageParam lpparam) throws Throwable {
-            PayUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.pay.ui." + new String[]{"WalletPayUI","WalletPayUI","WalletPayUI","WalletPayUI"}[idx], lpparam.classLoader);
-            Payview = XposedHelpers.findClass("com.tencent.mm.plugin.wallet_core.ui." + new String[]{"l","l","l","l"}[idx], lpparam.classLoader);
-            FetchUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.balance.ui." + new String[]{"WalletBalanceFetchPwdInputUI","WalletBalanceFetchPwdInputUI","WalletBalanceFetchPwdInputUI","WalletBalanceFetchPwdInputUI"}[idx], lpparam.classLoader);
-            WalletBaseUI = XposedHelpers.findClass("com.tencent.mm.wallet_core.ui." + new String[]{"WalletBaseUI","WalletBaseUI","WalletBaseUI","WalletBaseUI"}[idx], lpparam.classLoader);
+            switch(idx){
+                case 0:case 1:case 2:case 3:case 4:
+                    PayUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.pay.ui.WalletPayUI", lpparam.classLoader);
+                    Payview = XposedHelpers.findClass("com.tencent.mm.plugin.wallet_core.ui.l", lpparam.classLoader);
+                    FetchUI = XposedHelpers.findClass("com.tencent.mm.plugin.wallet.balance.ui.WalletBalanceFetchPwdInputUI", lpparam.classLoader);
+                    WalletBaseUI = XposedHelpers.findClass("com.tencent.mm.wallet_core.ui.WalletBaseUI", lpparam.classLoader);
+            }
         }
     }
 
@@ -26,11 +29,38 @@ public class ObfuscationHelper {
         public static String Passwd_Text;
 
         private static void init(int idx) throws Throwable {
-            PaypwdView = new String[]{"qVO","ryk","ryM","rLB"}[idx];
-            PaypwdEditText = new String[]{"vyO","wjm","wjX","wDJ"}[idx];
-            PayInputView = new String[]{"mOL","nnG","nnZ","nol"}[idx];
-            PayTitle = new String[]{"qVK","ryg","ryI","rLw"}[idx];
-            Passwd_Text = new String[]{"qVK","ryz","rzb","rLQ"}[idx];
+            switch(idx){
+                case 0:
+                    PaypwdView = "qVO";
+                    PaypwdEditText ="vyO" ;
+                    PayInputView = "mOL";
+                    PayTitle = "qVK";
+                    Passwd_Text = "qVK";
+                case 1:
+                    PaypwdView = "ryk";
+                    PaypwdEditText ="wjm" ;
+                    PayInputView = "nnG";
+                    PayTitle = "ryg";
+                    Passwd_Text = "ryz";
+                case 2:
+                    PaypwdView = "ryM";
+                    PaypwdEditText ="wjX" ;
+                    PayInputView = "nnZ";
+                    PayTitle = "ryI";
+                    Passwd_Text = "rzb";
+                case 3:
+                    PaypwdView = "rLB";
+                    PaypwdEditText ="wDJ" ;
+                    PayInputView = "nol";
+                    PayTitle = "rLw";
+                    Passwd_Text = "rLQ";
+                case 4:
+                    PaypwdView = "rWo";
+                    PaypwdEditText ="xhU" ;
+                    PayInputView = "nzg";
+                    PayTitle = "rWj";
+                    Passwd_Text = "rWD";
+            }
         }
     }
     public static class MM_Res {
@@ -39,9 +69,28 @@ public class ObfuscationHelper {
         public static int passwd_title;
 
         private static void init(int idx) throws Throwable {
-            Finger_icon=new int[]{2130838280,2130838289,2130838289,2130838298}[idx];
-            Finger_title=new int[]{2131236833,2131236918,2131236918,2131236964}[idx];
-            passwd_title=new int[]{2131236838,2131236923,2131236923,2131236969}[idx];
+            switch (idx){
+                case 0:
+                    Finger_icon = 2130838280;
+                    Finger_title = 2131236833;
+                    passwd_title = 2131236838;
+                case 1:
+                    Finger_icon = 2130838289;
+                    Finger_title = 2131236918;
+                    passwd_title = 2131236923;
+                case 2:
+                    Finger_icon = 2130838289;
+                    Finger_title = 2131236918;
+                    passwd_title = 2131236923;
+                case 3:
+                    Finger_icon = 2130838298;
+                    Finger_title = 2131236964;
+                    passwd_title = 2131236969;
+                case 4:
+                    Finger_icon = 2130838248;
+                    Finger_title = 2131237043;
+                    passwd_title = 2131237048;
+            }
         }
     }
 
@@ -75,6 +124,10 @@ public class ObfuscationHelper {
         if(versionName.contains("6.5.13") && versioncode == 1100){
             versionint=3;
             return 3;
+        }
+        if(versionName.contains("6.5.16") && versioncode == 1120){
+            versionint=4;
+            return 4;
         }
         return -1;
     }
